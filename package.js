74f2278faf412b158d285278a270ab705c3716c3
phambanhan <pbn.tut@gmail.com>
2016-05-13 15:55:21 +0700

    Update package.js
    Add github url

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'phambanhan:xml2js',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'XML to JavaScript object converter',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/phambanhan/meteor-xml2js',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
